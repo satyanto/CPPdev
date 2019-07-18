82f419c908af11a6738f930cc5d78ef8d65cae3d
Hafidh Satyanto <hafidhsatyanto@Hafidhs-MacBook-Pro.local>
2019-07-18 10:05:44 -0500

    simple output test

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# CCCPwork
+# CCCP lol
+
+learning c plus plus. 
+
+
+
+
+
+to compile on MacOS:
+```g++ -o [name of executable] [cpp file name].cpp```
+
+to run on MacOS terminal:
+```./[name of executable]```
---
+++ simpletest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+//  using namespace std;
+
+/* You can either use the 'using namespace std' above or use the std:: operator. Either way, you have to use it to use 'cout'. */
+
+int main() {
+    std::cout << "Hello world! \n";
+    std::cout << "I am trying to learn C++ \n";
+
+    return 0;
+}
+
